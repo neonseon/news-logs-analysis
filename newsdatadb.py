6e8c95fe3eab5cd0ce0361675b837803e12cdab6
Seon Ricks <skcirs@gmail.com>
2018-12-04 17:45:08 -0800

    refactor: Use triple quotation marks notation for multi-line queries

--- newsdatadb.py
+++ newsdatadb.py
@@ -11,10 +11,15 @@ def get_popular_articles():
     """Return the most popular three articles of all time."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select initcap(articles.title), num from (select path," +
-              " count(*) as num from log group by path order by num desc)" +
-              " log join articles on" +
-              " (log.path like '%' || articles.slug) limit 3")
+    c.execute("""
+      SELECT initcap(articles.title), num
+      FROM (SELECT path, count(*) AS num
+      FROM log
+      GROUP BY path
+      ORDER BY num DESC) log JOIN articles
+      ON (log.path LIKE '%' || articles.slug)
+      LIMIT 3;
+    """)
     articles = c.fetchall()
     db.close()
     print "\nMost popular articles of all time:"
@@ -26,12 +31,17 @@ def get_popular_authors():
     """Return the most popular authors of all time."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select authors.name, sum(num) as views from" +
-              " (select path, count(*) as num from log group by path" +
-              " order by num desc) log" +
-              " join articles on (log.path like '%' || articles.slug)" +
-              " join authors on authors.id = articles.author" +
-              " group by authors.name order by views desc")
+    c.execute("""
+      SELECT authors.name, sum(num) AS views
+      FROM (SELECT path, count(*) AS num
+      FROM log
+      GROUP BY path
+      ORDER BY num DESC) log JOIN articles
+      ON (log.path LIKE '%' || articles.slug) JOIN authors
+      ON authors.id = articles.author
+      GROUP BY authors.name
+      ORDER BY views DESC;
+    """)
     authors = c.fetchall()
     db.close()
     print "\nMost popular authors of all time:"
@@ -43,15 +53,21 @@ def get_error_days():
     """Return days where over 1% of requests were errors."""
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    c.execute("select to_char(time::date, 'FMMonth FMDD, YYYY') as date," +
-              " round(percentage, 2) as errors from (select log.time::date," +
-              " (error::decimal/success * 100.0) as percentage" +
-              " from (select time::date, count(*) as success from log" +
-              " where status = '200 OK' group by time::date" +
-              " order by time::date) log join errortable" +
-              " on log.time::date = errortable.time::date" +
-              " group by log.time::date, errortable.error, log.success" +
-              " order by log.time::date) as total where percentage > 1")
+    c.execute("""
+      SELECT to_char(time::date, 'FMMonth FMDD, YYYY')
+      AS date, round(percentage, 2) AS errors
+      FROM (SELECT log.time::date,(error::decimal/success * 100.0)
+      AS percentage
+      FROM (SELECT time::date, count(*) AS success
+      FROM log
+      WHERE status = '200 OK'
+      GROUP BY time::date
+      ORDER BY time::date) log JOIN errortable
+      ON log.time::date = errortable.time::date
+      GROUP BY log.time::date, errortable.error, log.success
+      ORDER BY log.time::date) AS total
+      WHERE percentage > 1
+    """)
     days = c.fetchall()
     db.close()
     print "\nDays where over 1% of requests were errors:"
